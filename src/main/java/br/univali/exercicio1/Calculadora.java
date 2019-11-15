547a7d3a9d7478fd3b5580625120dc1c7dca2a0d
Jeferson Viana Perito <jeferson.perito@softplan.com.br>
2019-11-15 16:01:09 -0300

    Update Calculadora.java

--- src/main/java/br/univali/exercicio1/Calculadora.java
+++ src/main/java/br/univali/exercicio1/Calculadora.java
@@ -5,4 +5,20 @@ public class Calculadora {
     public int soma(int a, int b) {
         return a + b;
     }
+
+    public int subtrai(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplica(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int dividendo, int divisor) {
+        if (divisor == 0) {
+            throw new IllegalArgumentException("Divisor não pode ser zero");
+        }
+
+        return dividendo / divisor;
+    }
 }
