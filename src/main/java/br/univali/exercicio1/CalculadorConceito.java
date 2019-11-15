899c2363cb58a93612125afc2530a19c81dbd540
Jeferson Viana Perito <jeferson.perito@softplan.com.br>
2019-11-15 16:24:09 -0300

    Create CalculadorConceito.java

---
+++ src/main/java/br/univali/exercicio1/CalculadorConceito.java
@@ -0,0 +1,23 @@
+package br.univali.exercicio1;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class CalculadorConceito {
+
+    /**
+     * Implemente um algoritmo que retorne o conceito baseado na nota
+     *
+     *  |     Nota     | Conceito
+     *  --------------------------
+     *  | 9,0	       | A
+     *  | 7,5 e < 9,0  | B
+     *  | 6,0 e < 7,5  | C
+     *  | < 6,0        | D
+     *
+     *  OBS 1: não deve permitir nota abaixo de 0.
+     *  OBS 2: não deve permitir nota acima de 10.
+     */
+    public char calculaConceitoBaseadoNaNota(double nota) {
+        throw new NotImplementedException();
+    }
+}
